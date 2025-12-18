44aa9374286d6a371ec2bcae454628c128b998c4
Emmanouil Koukoularis <koukoularisman@gmail.com>
2025-12-18 13:44:52 +0200

    added setup script to separate from install

--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -3,8 +3,6 @@
 ROOT=$(pwd)
 LIB="$ROOT/lib/wallet-common"
 
-cp scripts/wallet-backend-server-config.ts wallet-backend-server/config/index.ts
-cp scripts/wallet-frontend-env wallet-frontend/.env
 
 (cd "$LIB" && yarn install --frozen-lockfile && yarn build && rm -rf node_modules)
 
@@ -16,18 +14,3 @@ for dir in */ ; do
 done
 
 wait
-
-# copy issuer certificates and keys
-mkdir -p ./wallet-issuer/certs/
-cp scripts/keystore/wwwallet_org_iaca.pem ./wallet-issuer/certs/wwwallet_org_iaca.pem
-cp scripts/keystore/wwwallet_org_iaca.pem ./wallet-issuer/keys/ca.crt
-cp scripts/keystore/example_wwwallet_org.pem ./wallet-issuer/keys/pem.crt
-cp scripts/keystore/example_wwwallet_org.key.pkcs8 ./wallet-issuer/keys/pem.key
-
-cd wallet-issuer/
-yarn cmd:ecdh-keygen
-yarn cmd:hs512-keygen
-cd ../
-
-cp ./wallet-issuer/.env.template ./wallet-issuer/.env
-
---
+++ scripts/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cp scripts/wallet-backend-server-config.ts wallet-backend-server/config/index.ts
+cp scripts/wallet-frontend-env wallet-frontend/.env
+
+# copy issuer certificates and keys
+rm -rf ./wallet-issuer/certs/ ./wallet-issuer/keys/
+mkdir -p ./wallet-issuer/certs/
+mkdir -p ./wallet-issuer/keys/
+
+cp scripts/keystore/wwwallet_org_iaca.pem ./wallet-issuer/certs/wwwallet_org_iaca.pem
+cp scripts/keystore/wwwallet_org_iaca.pem ./wallet-issuer/keys/ca.crt
+cp scripts/keystore/example_wwwallet_org.pem ./wallet-issuer/keys/pem.crt
+cp scripts/keystore/example_wwwallet_org.key.pkcs8 ./wallet-issuer/keys/pem.key
+
+cd wallet-issuer/
+yarn cmd:ecdh-keygen
+yarn cmd:hs512-keygen
+cd ../
+
+cp ./wallet-issuer/.env.template ./wallet-issuer/.env
+
