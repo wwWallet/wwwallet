501e88b5320a0963514ca55c018330cbd1ccaac0
kkmanos <koukoularisman@gmail.com>
2026-01-13 17:06:17 +0200

    add builder script

---
+++ scripts/build.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+contains() {
+  local e match="$1"
+  shift
+  for e; do
+    [[ "$e" == "$match" ]] && return 0
+  done
+  return 1
+}
+
+images=()
+arch="linux/amd64"
+push=0
+
+while getopts "hi:c:v:a:p" opt; do
+  case $opt in
+    h)
+      echo "Usage: $0 -i image-name1 [-i image-name2 ...] -c context -v version -a architecture -p"
+      exit 0
+      ;;
+    i)
+      images+=("$OPTARG")
+      ;;
+    c)
+      context="$OPTARG"
+      ;;
+    p)
+      push=1
+      ;;
+    v)
+      ver="$OPTARG"
+      ;;
+    a)
+      arch="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [[ -z "$ver" ]]; then
+  echo "Error: -v version is required" >&2
+  exit 1
+fi
+
+if [[ -z "$context" ]]; then
+  echo "Error: -c context is required" >&2
+  exit 1
+fi
+
+if [[ ${#images[@]} -eq 0 ]]; then
+  echo "Error: No images specified"
+  exit 1
+else
+  echo "Images: ${images[@]}"
+fi
+
+[ "$push" -eq 1 ] && push_flag="--push" || push_flag=""
+
+
+if contains "wallet-frontend" "${images[@]}"; then
+	cd wallet-frontend/
+	docker buildx build --platform $arch $push_flag -t ${context}/wallet-frontend:${ver} .
+	cd ..
+fi
+
+if contains "wallet-backend-server" "${images[@]}"; then
+	cd wallet-backend-server/
+	docker buildx build --platform $arch $push_flag -t ${context}/wallet-backend-server:${ver} .
+	cd ..
+fi
+
+if contains "wallet-as" "${images[@]}"; then
+	cd wallet-as/
+	docker buildx build --platform $arch $push_flag -t ${context}/wallet-as:${ver} .
+	cd ..
+fi
+
+if contains "wallet-issuer" "${images[@]}"; then
+	cd wallet-issuer/
+	docker buildx build --platform $arch $push_flag -t ${context}/wallet-issuer:${ver} .
+	cd ..
+fi
+
+if contains "wallet-verifier" "${images[@]}"; then
+	cd wallet-verifier/
+	docker buildx build --platform $arch $push_flag -t ${context}/wallet-verifier:${ver} .
+	cd ..
+fi
