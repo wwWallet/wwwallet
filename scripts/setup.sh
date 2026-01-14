28f40bee1e9bdc14a83330dbe156b2b0de15e55e
Panagiota Stamatopoulou <pstamatop@gmail.com>
2026-01-14 12:39:58 +0200

    Update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
+# Copy configuration and environment template files
 cp scripts/wallet-backend-server-config.ts wallet-backend-server/config/index.ts
 cp scripts/wallet-frontend-env wallet-frontend/.env
 cp wallet-issuer/.env.template wallet-issuer/.env
 cp wallet-as/.env.template wallet-as/.env
+cp wallet-verifier/config/config.template.ts wallet-verifier/config/config.development.ts
 
-# copy issuer and verifier certificates and keys
-rm -rf ./wallet-issuer/certs/ ./wallet-issuer/keys/
+# Create issuer certificates and keys
 rm -rf ./wallet-issuer/certs/ ./wallet-issuer/keys/
 mkdir -p ./wallet-issuer/certs/
 mkdir -p ./wallet-issuer/keys/
-
-rm -rf ./wallet-verifier/certs/ ./wallet-verifier/keys/
-rm -rf ./wallet-verifier/certs/ ./wallet-verifier/keys/
-mkdir -p ./wallet-verifier/keys/
 
 cp scripts/keystore/wwwallet_org_iaca.pem ./wallet-issuer/certs/wwwallet_org_iaca.pem
 cp scripts/keystore/wwwallet_org_iaca.pem ./wallet-issuer/keys/ca.crt
@@ -25,12 +22,18 @@ yarn cmd:ecdh-keygen
 yarn cmd:hs512-keygen
 cd ../
 
+# Copy verifier certificates and keys
+rm -rf ./wallet-verifier/certs/ ./wallet-verifier/keys/
+mkdir -p ./wallet-verifier/certs/
+mkdir -p ./wallet-verifier/keys/
+
+cp scripts/keystore/wwwallet_org_iaca.pem ./wallet-verifier/certs/wwwallet_org_iaca.pem
 cp scripts/keystore/example_wwwallet_org.key.pkcs8 ./wallet-verifier/keys/pem.key
 cp scripts/keystore/wwwallet_org_iaca.pem ./wallet-verifier/keys/ca.crt
 cp scripts/keystore/example_wwwallet_org.pem ./wallet-verifier/keys/pem.crt
 
-cp ./wallet-verifier/config/config.template.ts ./wallet-verifier/config/config.development.ts
 
+# Copy wallet backend server certificates and keys
 mkdir -p ./wallet-backend-server/keys/
 cp scripts/keystore/example_wwwallet_org.key.pkcs8 ./wallet-backend-server/keys/wallet-provider.key
 cp scripts/keystore/example_wwwallet_org.pem ./wallet-backend-server/keys/wallet-provider.pem
