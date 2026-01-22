fb8072f880bbb69e14202402db027610710677f5
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-01-22 13:03:17 +0200

    chrore: which yarn start to dev frontend

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -24,7 +24,7 @@ done
 yarn concurrently \
     --names "wallet-frontend,wallet-backend-server,wallet-issuer,wallet-verifier,vct-registry,wallet-as" \
     --prefix-colors "blueBright,greenBright,yellowBright,magentaBright,cyanBright,blue" \
-    "npm --prefix wallet-frontend run start" \
+    "npm --prefix wallet-frontend run dev" \
     "npm --prefix wallet-backend-server run dev" \
     "npm --prefix wallet-issuer run dev" \
     "npm --prefix wallet-verifier run dev" \
