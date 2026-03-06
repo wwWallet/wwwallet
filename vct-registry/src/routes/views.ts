1694cc27cbcf3082fdeba40c0db6b51abd2afc3a
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-03-07 00:11:46 +0200

    refactor(vct-registry): migrate views from html to nunjucks templates

--- vct-registry/src/routes/views.ts
+++ vct-registry/src/routes/views.ts
@@ -1,26 +1,24 @@
 import { Router } from "express";
 import { authView } from "../middleware/auth";
 import { config } from "../../config";
-import path from "path";
-import { viewsPath } from "../server";
 
 /** / */
 const viewsRouter = Router();
 
 viewsRouter.get("/", (_req, res) => {
-	res.render(path.join(viewsPath, "index.html"), {
+	res.render("index.njk", {
 		baseHref: config.base_url,
 	});
 });
 
 viewsRouter.get("/add", authView, (_req, res) => {
-	res.render(path.join(viewsPath, "add.html"), {
+	res.render("add.njk", {
 		baseHref: config.base_url,
 	});
 });
 
 viewsRouter.get("/edit", authView, (_req, res) => {
-	res.render(path.join(viewsPath, "edit.html"), {
+	res.render("edit.njk", {
 		baseHref: config.base_url,
 	});
 });
